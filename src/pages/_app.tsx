541f1b64735ba73e02926cfc37c651dc56d14c07
sakanobu <grapejuice100percent@gmail.com>
2021-11-24 11:53:53 +0900

    tailswindcss の初期設定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,12 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-// eslint-disable-next-line import/no-extraneous-dependencies
+import 'tailwindcss/tailwind.css';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Head>
-        {/* eslint-disable-next-line react/jsx-sort-props */}
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
       </Head>
       {/* eslint-disable-next-line react/jsx-props-no-spreading */}
       <Component {...pageProps} />
